864f4d3b139c3ddb6ee5c6eeb0ec1b880c771dcd
Ryan Young <lightning16ry@gmail.com>
2023-10-20 19:20:55 -0500

    Squashed commit of the following:
    commit d9a5ffb4f2fb2d6bf5e4d53cc61ce4b670a9c519
    Author: Ryan Young <lightning16ry@gmail.com>
    Date:   Fri Oct 20 19:20:45 2023 -0500
    
        Successful refactor of substitution extraction
    
        using given API substitution data instead of self-generated ones

--- src/jobs_/create_subs_up_to_date/main.py
+++ src/jobs_/create_subs_up_to_date/main.py
@@ -53,18 +53,6 @@ def create_subs_up_to_date(
                         game_id=game_id,
                         league_id=0,
                     ).get_data_frames()
-                    time.sleep(2)
-
-                    game_pbp = playbyplayv2.PlayByPlayV2(
-                            timeout=timeout,
-                            game_id=game_id).get_data_frames()[0]
-                    time.sleep(2)
-
-                    game_box_score = boxscoreadvancedv2.BoxScoreAdvancedV2(
-                            timeout=timeout,
-                            game_id=game_id,
-                        ).get_data_frames()[0]
-                    time.sleep(2)
 
                     team_ids = [
                         game_rosters[0].loc[0, 'TEAM_ID'],
@@ -72,62 +60,20 @@ def create_subs_up_to_date(
                     ]
 
                     for team_id in team_ids:
-                        roster = \
+                        roster_obj = \
                             next(x for x in game_rosters if team_id in x.loc[:1, 'TEAM_ID'].values) \
-                                .loc[:, ['PERSON_ID', 'PLAYER_FIRST', 'PLAYER_LAST']].drop_duplicates()
+                                .loc[:, ['PERSON_ID', 'PLAYER_FIRST', 'PLAYER_LAST', 'IN_TIME_REAL', 'OUT_TIME_REAL']]
 
-                        roster_subs_per_game = roster.apply(
+                        players = roster_obj.loc[:, ['PERSON_ID', 'PLAYER_FIRST', 'PLAYER_LAST',]].drop_duplicates()
+                        roster_subs_per_game = players.apply(
                             lambda a: {'PLAYER_ID': a.PERSON_ID,
-                                       'SUBS': [], 'PLAYER_NAME': (a.PLAYER_FIRST + " " + a.PLAYER_LAST),
+                                       'SUBS': list(map(int,
+                                                        pd.Series(roster_obj.loc[roster_obj.PERSON_ID == a.PERSON_ID, ['IN_TIME_REAL', 'OUT_TIME_REAL']]
+                                                                  .values.tolist()).explode().tolist())),
+                                       'PLAYER_NAME': (a.PLAYER_FIRST + " " + a.PLAYER_LAST),
                                        'GAME_ID': game_id},
                             axis=1).tolist()
-
-                        game_subs = game_pbp.loc[(game_pbp.EVENTMSGTYPE == 8) & (game_pbp.PLAYER1_TEAM_ID == team_id)]
-
-                        players = game_box_score[game_box_score.TEAM_ID == team_id][['PLAYER_NAME', 'PLAYER_ID']]
-                        players.set_index('PLAYER_ID', inplace=True)
-
-                        for period in [1, 2, 3, 4]:
-
-                            subs_this_period = game_subs.loc[(game_pbp.PERIOD == period)]
-
-                            def init_quarter(row):
-                                total_subs = subs_this_period.loc[
-                                    (subs_this_period.PLAYER1_ID == row.name) | (subs_this_period.PLAYER2_ID == row.name)]
-                                if len(total_subs) == 0:
-                                    return []
-
-                                first_sub = total_subs.iloc[0]
-                                if first_sub.PLAYER1_ID == row.name:
-                                    return [(period - 1) * 720]
-                                return []
-
-                            player_subs_i = players.apply(
-                                lambda a: {'PLAYER_ID': a.name, 'SUBS': init_quarter(a)}, axis=1) \
-                                .tolist()
-
-                            def sub_iter(sub):
-                                next(i for i in player_subs_i if i['PLAYER_ID'] == sub.PLAYER1_ID) \
-                                    ['SUBS'].append(calculate_seconds_elapsed_in_period(period, sub.PCTIMESTRING))
-                                next(j for j in player_subs_i if j['PLAYER_ID'] == sub.PLAYER2_ID) \
-                                    ['SUBS'].append(calculate_seconds_elapsed_in_period(period, sub.PCTIMESTRING))
-
-                            subs_this_period.apply(sub_iter, axis=1)
-
-                            def end_of_quarter(obj):
-                                subs_i = obj['SUBS']
-                                if bool(len(subs_i) % 2): \
-                                        obj['SUBS'].append(period * 720)
-                                return {'PLAYER_ID': obj['PLAYER_ID'], 'SUBS': obj['SUBS']}
-
-                            player_subs_i = list(map(end_of_quarter, player_subs_i))
-
-                            for p in player_subs_i:
-                                player_i = next((i for i in roster_subs_per_game if i['PLAYER_ID'] == p['PLAYER_ID']), None)
-                                if player_i != None:
-                                    player_i['SUBS'] += p['SUBS']
-                            retries = retry_attempts
-                        break
+                        retries = retry_attempts
                 except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as error:
                     print('Request Error')
                     # print(error)
@@ -138,9 +84,9 @@ def create_subs_up_to_date(
                         raise Exception('Too many API timeouts')
                     continue
                 sub_list += roster_subs_per_game
+                break
 
-        list(map(lambda a: process_game(a, roster_subs), game_series[0:2]))
-
+        list(map(lambda a: process_game(a, roster_subs), game_series))
 
         game_df = pd.DataFrame(roster_subs)
         game_df.to_csv('../../../data/schedule_test.json')
@@ -153,4 +99,3 @@ if __name__ == '__main__':
     result = False
     while not result:
         result = create_subs_up_to_date(date='10/16/2023', season_id='2023-24', season_type='Pre Season')
-        time.sleep(5)
--- src/schedule/main.py
+++ src/schedule/main.py
@@ -1,21 +1,27 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from src.jobs_.create_subs_up_to_date.main import create_subs_up_to_date as create
 import time
+from generic.save_last_modified.main import load_date, store_date
 
 sched = BlockingScheduler()
 
 season_id = '2023-24'
-season_type= ['Pre Season', 'Regular Season', 'Playoffs']
+season_type = ['Pre Season', 'Regular Season', 'Playoffs']
 
-# @sched.scheduled_job('cron', day_of_week='mon-sun', hour=5)
-# def scheduled_job():
-#     result = False
-#     while not result:
-#         result = create(date="10/16/2023", season_id=season_id, season_type=season_type[0])
-#         time.sleep(5)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=5)
+def scheduled_job():
+    result = False
+    last_modified = load_date()
+    try:
+        while not result:
+            result = create(date=last_modified, season_id=season_id, season_type=season_type[0])
+            time.sleep(5)
+    except Exception as error:
+        return
+    store_date()
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
+# @sched.scheduled_job('interval', minutes=3)
+# def timed_job():
+#     print('This job is run every three minutes.')
 
 sched.start()
